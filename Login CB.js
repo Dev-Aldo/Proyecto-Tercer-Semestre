3d6cabb6aa0abf8aab00b4e0865e930c6c567fdb
Dev-Aldo <aldo.ayala.alp@cbtis258.edu.mx>
2024-10-24 04:47:16 -0600

    Add files via upload

---
+++ Login CB.js
@@ -0,0 +1,95 @@
+alert("Pagina en desarrollo")
+//Animacion para el login
+
+//Ejecutando funciones
+document.getElementById("btn_iniciar-sesion").addEventListener("click", iniciarSesion);
+document.getElementById("btn_registrarse").addEventListener("click", register);
+window.addEventListener("resize", anchoPage);
+
+//Declarando variables
+var formulario_login = document.querySelector(".formulario_login");
+var formulario_register = document.querySelector(".formulario_register");
+var contenedor_login_register = document.querySelector(".contenedor_login-register");
+var caja_trasera_login = document.querySelector(".caja_trasera-login");
+var caja_trasera_register = document.querySelector(".caja_trasera-register");
+
+    //Funciones
+
+function anchoPage(){
+
+    if (window.innerWidth > 850){
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display = "block";
+    }else{
+        caja_trasera_register.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.display = "none";
+        formulario_login.style.display = "block";
+        contenedor_login_register.style.left = "0px";
+        formulario_register.style.display = "none";   
+    }
+}
+
+anchoPage();
+
+
+    function iniciarSesion(){
+        if (window.innerWidth > 850){
+            formulario_login.style.display = "block";
+            contenedor_login_register.style.left = "10px";
+            formulario_register.style.display = "none";
+            caja_trasera_register.style.opacity = "1";
+            caja_trasera_login.style.opacity = "0";
+        }else{
+            formulario_login.style.display = "block";
+            contenedor_login_register.style.left = "0px";
+            formulario_register.style.display = "none";
+            caja_trasera_register.style.display = "block";
+            caja_trasera_login.style.display = "none";
+        }
+    }
+
+    function register(){
+        if (window.innerWidth > 850){
+            formulario_register.style.display = "block";
+            contenedor_login_register.style.left = "410px";
+            formulario_login.style.display = "none";
+            caja_trasera_register.style.opacity = "0";
+            caja_trasera_login.style.opacity = "1";
+        }else{
+            formulario_register.style.display = "block";
+            contenedor_login_register.style.left = "0px";
+            formulario_login.style.display = "none";
+            caja_trasera_register.style.display = "none";
+            caja_trasera_login.style.display = "block";
+            caja_trasera_login.style.opacity = "1";
+        }
+}
+//Cierre de las funciones para la animacion de login
+
+//Autentificacion
+
+let validEmail = "si"
+let validPass = "no";
+
+const login = (event) =>{
+    event.preventDefault();
+
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const mensaje = document.getElementById("mensaje");
+
+    if(email === validEmail && password === validPass){
+        mensaje.textContent = "inicio de sesion exitoso";
+        mensaje.style.color = "green";
+
+        setTimeout(() => {
+            window.location.href = "index.html";
+        }, 1000 );
+    }
+    else{
+        mensaje.textContent = "Correo o contraseña incorrecta";
+        mensaje.style.color = "red";
+    }
+}
